755b6ce61c7429740bd51d8c05d5ac593b9486b5
Everson <joseeverson757@gmail.com>
2025-05-15 13:02:42 -0300

    D2 - Modelos e Banco de Dados
    Criação dos Models principais.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_one :cart, dependent: :destroy
+  has_many :orders, dependent: :destroy
+
+  after_create :create_cart
+
+  private
+
+  def create_cart
+    Cart.create(user: self)
+  end
 end
